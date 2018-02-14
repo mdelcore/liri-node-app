ee0655512232b11cd7dd6f2bd697d0c6e50f822f
Michael Delcore <nmdelcore@gmail.com>
2018-02-14 08:28:56 -0700

    project files added

---
+++ liri.js
@@ -0,0 +1,7 @@
+// dependencies stored as variables
+var keys = require('./key.js');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
